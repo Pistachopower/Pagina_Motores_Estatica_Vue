813dc538a10d415e579ff0a359f19ed3abcc8737
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2025-06-10 15:47:09 +0200

    instalacion y funcionamiento de bootstrap y estructura base para trabajar

--- Pagina_Motores_Estatica_Vue/src/App.vue
+++ Pagina_Motores_Estatica_Vue/src/App.vue
@@ -1,24 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-//import HelloWorld from './components/HelloWorld.vue'
+import HomeView from './views/HomeView.vue'
 </script>
 
 <template>
-  <header>
-  
-    <div class="wrapper">
-      
-
-      <nav>
-        <!-- <RouterLink to="/">Home</RouterLink> -->
-
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
+  <HomeView/>
 </template>
 
 <style scoped>
-
 </style>
---
+++ Pagina_Motores_Estatica_Vue/src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+   <div class="container mt-5">
+    <h1 class="text-primary">¡Bootstrap está funcionando!</h1>
+    <button class="btn btn-success">Botón de prueba</button>
+  </div>
+</template>
+
+<style>
+</style>
