c1c1223c89a42d931f056def24b937f8358f81db
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2026-09-17 18:25:45 +0200

    Configurar despliegue en GitHub Pages

--- Pagina_Motores_Estatica_Vue/vite.config.js
+++ Pagina_Motores_Estatica_Vue/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Pagina_Motores_Estatica_Vue/',
   plugins: [
     vue(),
     vueDevTools(),
